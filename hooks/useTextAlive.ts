a270a4f7de28e2b6a6b5c98fca62f2708fbb91b7
Kz <aikikizuki@gmail.com>
2025-05-11 21:45:41 +0900

    ✨ useTextAliveフックを新たに実装し、TextAliveの初期化とリスナー作成の機能を追加しました。

---
+++ hooks/useTextAlive.ts
@@ -0,0 +1,3 @@
+'use client'
+import { useEffect, useRef, useState } from 'react'
+import { createTextAlivePlayer, initTextAlive, createTextAliveListener } from '@/lib/textAlive'
--- lib/textAlive.ts
+++ lib/textAlive.ts
@@ -1,17 +1,36 @@
 import { Player, PlayerListener, IPhrase, IWord } from "textalive-app-api"
-import { SongConfig } from "@/types/song"
 
-let player: Player | null = null
-let phrase: IPhrase | null = null
-
-let mediaElementRef: React.RefObject<HTMLVideoElement> | null = null
-
-export const initTextAlive = (config: SongConfig) => {
-  if (player) {
-    return
-  }
-
-  player = new Player()
-  player.load(config.songUrl)
-  
+export const initTextAlive = (mediaElement: HTMLVideoElement) => {
+  return new Player({
+    app: { token: process.env.NEXT_PUBLIC_TEXTALIVE_API_KEY! },
+    mediaElement,
+  })
 }
+
+export const createTextAliveListener = (
+  setPhrase: (phrase: any) => void,
+  setPlayer: (player: Player) => void,
+  setBeat: (beat: number) => void,
+  delay: number,
+): PlayerListener => ({
+  onTimerReady: function () {
+    let phrase = this.video.firstPhrase
+    while(phrase) {
+      phrase.animate = (now: any, unit: any) => {
+        if (unit.startTime - delay <= now && unit.endTime + delay > now) {
+          setPhrase(unit)
+        }
+      }
+
+      if(phrase.next === null) break
+      phrase = phrase.next
+    }
+    setPlayer(this)
+  },
+  onTimeUpdate: function (position: any) {
+    if(!this.video.findPhrase(position)) {
+      setPhrase(undefined)
+    }
+    setBeat(this.findBeat(position + delay) ?? undefined)
+  },
+})
