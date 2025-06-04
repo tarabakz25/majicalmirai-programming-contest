26337801cb2ec188b5abee08d7f038c4c93d08c4
Kizuki Aiki <aikikizuki@gmail.com>
2025-06-04 19:54:54 +0900

    ✨ HomePageコンポーネントにスタートボタンを追加し、クリック時の状態管理を実装しました。また、MusicProviderの表示ロジックを改善しました。gameページを新たに作成しました。

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react"
 import { useState } from "react"
 import Link from "next/link"
 import { redirect } from "next/navigation"
-import Gsap from 'gsap'
+import Gsap, { set } from 'gsap'
 import { MusicProvider } from "@/components/MusicProvider"
 import { SelectSongs } from "@/components/SelectSongs"
 import { LyricsDisplay } from "@/components/LyricsDisplay"
@@ -13,38 +13,29 @@ import { SongConfig } from "@/types/song"
 const titleText = "LirycRails!"
 
 export default function HomePage() {
+  const [isClickedStart, setIsClickedStart] = useState(false);
   const [isMusicSelected, setIsMusicSelected] = useState(false);
 
+  function handleClickStart(e: any) {
+    e.preventDefault();
+    setIsClickedStart(true);
+  }
 
-  useEffect(() => {
-    Gsap.set(".title-char", { opacity: 0, x: 0 })
-    Gsap.set(".start-btn", { opacity: 0, x: 0 })
-    Gsap.to(".title-char", {
-      opacity: 1,
-      ease: "power2.inOut",
-      delay: 0.5
-    })
-    Gsap.to(".start-btn", {
-      opacity: 1,
-      ease: "power2.inOut",
-      delay: 2
-    })
-  }, [])
+  function handleClickMusic(e: any) {
+    e.preventDefault();
+    setIsMusicSelected(true);
+  }
+
+  if (!isClickedStart) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">LirycRails!</h1>
+        <button className="start-btn mt-4 px-4 py-2  text-white rounded-md" onClick={handleClickStart}>Touch to Start</button>
+      </div>
+    )
+  }
 
   if (!isMusicSelected) {
     return <MusicProvider />;
   }
-
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-white text-black">
-      <h1 className="text-4xl font-bold">
-        {titleText.split("").map((char, i) => (
-          <span key={i} className="title-char inline-block">{char}</span>
-        ))}
-      </h1>
-      <button className="start-btn mt-4 px-4 py-2  text-white rounded-md" onClick={() => redirect("/game")}>Touch to Start</button>
-    </div>
-  )
-
-  return <MusicProvider />;
 }
